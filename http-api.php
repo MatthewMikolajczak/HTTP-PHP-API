34dbe5928bd8e26626c437717fb5a1d35bb1cdb1
Mateusz Mikolajczak <mateusz.mikolajczak@yahoo.com>
2018-01-05 02:07:37 +0100

    Add files via upload

--- http-api.php
+++ http-api.php
@@ -89,7 +89,7 @@ else {
                 break;
             }
 
-            echo '{';
+            $stats = array(); // array for all results
 
             $res = $mysqli->query('SELECT * FROM `players` WHERE `gameID` = "' . "$game" . '"');
             if($res) {
@@ -100,9 +100,10 @@ else {
                     http_response_code(200); // good result
                 }
                 if(!empty($rows)) {
-                    echo 'players: ';
-                    echo(json_encode($rows));
-                    echo ', ';
+
+                    // array_push($stats, $rows);
+                    $stats[players] = $rows;
+
                 }
             }
             else http_response_code(500); // query doesn't work
@@ -116,9 +117,10 @@ else {
                     http_response_code(200); // good result
                 }
                 if(!empty($rows)) {
-                    echo 'serve: ';
-                    echo(json_encode($rows));
-                    echo ', ';
+
+                    // array_push($stats, $rows);
+                    $stats[serve] = $rows;
+
                 }
             }
             else http_response_code(500); // query doesn't work
@@ -132,9 +134,10 @@ else {
                     http_response_code(200); // good result
                 }
                 if(!empty($rows)) {
-                    echo 'reception: ';
-                    echo(json_encode($rows));
-                    echo ', ';
+
+                    // array_push($stats, $rows);
+                    $stats[reception] = $rows;
+
                 }
             }
             else http_response_code(500); // query doesn't work
@@ -148,9 +151,10 @@ else {
                     http_response_code(200); // good result
                 }
                 if(!empty($rows)) {
-                    echo 'attack: ';
-                    echo(json_encode($rows));
-                    echo ', ';
+
+                    // array_push($stats, $rows);
+                    $stats[attack] = $rows;
+
                 }
             }
             else http_response_code(500); // query doesn't work
@@ -164,9 +168,10 @@ else {
                     http_response_code(200); // good result
                 }
                 if(!empty($rows)) {
-                    echo 'block: ';
-                    echo(json_encode($rows));
-                    echo ', ';
+
+                    // array_push($stats, $rows);
+                    $stats[block] = $rows;
+
                 }
             }
             else http_response_code(500); // query doesn't work
@@ -177,14 +182,16 @@ else {
                 if($row) {
                     $rows = array();
                     $rows[] = $row;
-                    echo 'setting: ';
-                    echo(json_encode($rows));
+
+                    // array_push($stats, $rows);
+                    $stats[setting] = $rows;
+
                     http_response_code(200); // good result
                 } else http_response_code(204); // empty result
             }
             else http_response_code(500); // query doesn't work
 
-            echo '}';
+            echo(json_encode($stats));
 
             break;
         case 'get-saved_players':
@@ -232,23 +239,25 @@ else {
         case 'create-stats':
             $user = $_POST['userID'];
             $gameName = $_POST['gameName'];
-            $scoreJSON = $_POST['scoreJSON'];//json
-            $score = json_decode($scoreJSON);
-            $playersJSON = $_POST['playersJSON'];//json
+            $scoreAJSON = $_POST['scoreA'];//json
+            $scoreA = json_decode($scoreAJSON);
+            $scoreBJSON = $_POST['scoreB'];//json
+            $scoreB = json_decode($scoreBJSON);
+            $playersJSON = $_POST['players'];//json
             $players = json_decode($playersJSON);
-            $serveJSON = $_POST['serveJSON'];//json
+            $serveJSON = $_POST['serve'];//json
             $serve = json_decode($serveJSON);
-            $receptionJSON = $_POST['receptionJSON'];//json
+            $receptionJSON = $_POST['reception'];//json
             $reception = json_decode($receptionJSON);
-            $attackJSON = $_POST['attackJSON'];//json
+            $attackJSON = $_POST['attack'];//json
             $attack = json_decode($attackJSON);
-            $blockJSON = $_POST['blockJSON'];//json
+            $blockJSON = $_POST['block'];//json
             $block = json_decode($blockJSON);
-            $settingJSON = $_POST['settingJSON'];//json
+            $settingJSON = $_POST['setting'];//json
             $setting = json_decode($settingJSON);
             $date = date("Y-m-d");
 
-            $sql = 'INSERT INTO games (userID, name, date) VALUES ("' . $user . '", "' . $gameName . '", "' . $date . '")';
+            $sql = 'INSERT INTO games (userID, name, date, set1a, set1b, set2a, set2b, set3a, set3b, set4a, set4b, set5a, set5b) VALUES ("' . $user . '", "' . $gameName . '", "' . $date . '", "' . $scoreA[0] . '", "' . $scoreB[0] . '", "' . $scoreA[1] . '", "' . $scoreB[1] . '", "' . $scoreA[2] . '", "' . $scoreB[2] . '", "' . $scoreA[3] . '", "' . $scoreB[3] . '", "' . $scoreA[4] . '", "' . $scoreB[4] . '")';
             if ($mysqli->query($sql) === TRUE) {
                 http_response_code(200); // success
             } else {
@@ -291,7 +300,7 @@ else {
             }
 
             for($i = 0; $i < count($serve); $i++) {
-                $sql = 'INSERT INTO serve (playerID, gameID, set1-0, set1-1, set1-2, set1-3, set2-0, set2-1, set2-2, set2-3, set3-0, set3-1, set3-2, set3-3, set4-0, set4-1, set4-2, set4-3, set5-0, set5-1, set5-2, set5-3) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $serve[$i]->{'s1-0'} . '", "' . $serve[$i]->{'s1-1'} . '", "' . $serve[$i]->{'s1-2'} . '", "' . $serve[$i]->{'s1-3'} . '", "' . $serve[$i]->{'s2-0'} . '", "' . $serve[$i]->{'s2-1'} . '", "' . $serve[$i]->{'s2-2'} . '", "' . $serve[$i]->{'s2-3'} . '", "' . $serve[$i]->{'s3-0'} . '", "' . $serve[$i]->{'s3-1'} . '", "' . $serve[$i]->{'s3-2'} . '", "' . $serve[$i]->{'s3-3'} . '", "' . $serve[$i]->{'s4-0'} . '", "' . $serve[$i]->{'s4-1'} . '", "' . $serve[$i]->{'s4-2'} . '", "' . $serve[$i]->{'s4-3'} . '", "' . $serve[$i]->{'s5-0'} . '", "' . $serve[$i]->{'s5-1'} . '", "' . $serve[$i]->{'s5-2'} . '", "' . $serve[$i]->{'s5-3'} . '")';
+                $sql = 'INSERT INTO serve (playerID, gameID, set10, set11, set12, set13, set20, set21, set22, set23, set30, set31, set32, set33, set40, set41, set42, set43, set50, set51, set52, set53) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $serve[$i]->{'s1-0'} . '", "' . $serve[$i]->{'s1-1'} . '", "' . $serve[$i]->{'s1-2'} . '", "' . $serve[$i]->{'s1-3'} . '", "' . $serve[$i]->{'s2-0'} . '", "' . $serve[$i]->{'s2-1'} . '", "' . $serve[$i]->{'s2-2'} . '", "' . $serve[$i]->{'s2-3'} . '", "' . $serve[$i]->{'s3-0'} . '", "' . $serve[$i]->{'s3-1'} . '", "' . $serve[$i]->{'s3-2'} . '", "' . $serve[$i]->{'s3-3'} . '", "' . $serve[$i]->{'s4-0'} . '", "' . $serve[$i]->{'s4-1'} . '", "' . $serve[$i]->{'s4-2'} . '", "' . $serve[$i]->{'s4-3'} . '", "' . $serve[$i]->{'s5-0'} . '", "' . $serve[$i]->{'s5-1'} . '", "' . $serve[$i]->{'s5-2'} . '", "' . $serve[$i]->{'s5-3'} . '")';
                 if ($mysqli->query($sql) === TRUE) {
                     http_response_code(200); // success
                 } else {
@@ -300,7 +309,7 @@ else {
             }
 
             for($i = 0; $i < count($reception); $i++) {
-                $sql = 'INSERT INTO reception (playerID, gameID, set1-0, set1-1, set1-2, set1-3, set1-4, set2-0, set2-1, set2-2, set2-3, set2-4, set3-0, set3-1, set3-2, set3-3, set3-4, set4-0, set4-1, set4-2, set4-3, set4-4, set5-0, set5-1, set5-2, set5-3, set5-4,) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $reception[$i]->{'s1-0'} . '", "' . $reception[$i]->{'s1-1'} . '", "' . $reception[$i]->{'s1-2'} . '", "' . $reception[$i]->{'s1-3'} . '", "' . $reception[$i]->{'s1-4'} . '", "' . $reception[$i]->{'s2-0'} . '", "' . $reception[$i]->{'s2-1'} . '", "' . $reception[$i]->{'s2-2'} . '", "' . $reception[$i]->{'s2-3'} . '", "' . $reception[$i]->{'s2-4'} . '", "' . $reception[$i]->{'s3-0'} . '", "' . $reception[$i]->{'s3-1'} . '", "' . $reception[$i]->{'s3-2'} . '", "' . $reception[$i]->{'s3-3'} . '", "' . $reception[$i]->{'s3-4'} . '", "' . $reception[$i]->{'s4-0'} . '", "' . $reception[$i]->{'s4-1'} . '", "' . $reception[$i]->{'s4-2'} . '", "' . $reception[$i]->{'s4-3'} . '", "' . $reception[$i]->{'s4-4'} . '", "' . $reception[$i]->{'s5-0'} . '", "' . $reception[$i]->{'s5-1'} . '", "' . $reception[$i]->{'s5-2'} . '", "' . $reception[$i]->{'s5-3'} . '", "' . $reception[$i]->{'s5-4'} . '")';
+                $sql = 'INSERT INTO reception (playerID, gameID, set10, set11, set12, set13, set14, set20, set21, set22, set23, set24, set30, set31, set32, set33, set34, set40, set41, set42, set43, set44, set50, set51, set52, set53, set54) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $reception[$i]->{'s1-0'} . '", "' . $reception[$i]->{'s1-1'} . '", "' . $reception[$i]->{'s1-2'} . '", "' . $reception[$i]->{'s1-3'} . '", "' . $reception[$i]->{'s1-4'} . '", "' . $reception[$i]->{'s2-0'} . '", "' . $reception[$i]->{'s2-1'} . '", "' . $reception[$i]->{'s2-2'} . '", "' . $reception[$i]->{'s2-3'} . '", "' . $reception[$i]->{'s2-4'} . '", "' . $reception[$i]->{'s3-0'} . '", "' . $reception[$i]->{'s3-1'} . '", "' . $reception[$i]->{'s3-2'} . '", "' . $reception[$i]->{'s3-3'} . '", "' . $reception[$i]->{'s3-4'} . '", "' . $reception[$i]->{'s4-0'} . '", "' . $reception[$i]->{'s4-1'} . '", "' . $reception[$i]->{'s4-2'} . '", "' . $reception[$i]->{'s4-3'} . '", "' . $reception[$i]->{'s4-4'} . '", "' . $reception[$i]->{'s5-0'} . '", "' . $reception[$i]->{'s5-1'} . '", "' . $reception[$i]->{'s5-2'} . '", "' . $reception[$i]->{'s5-3'} . '", "' . $reception[$i]->{'s5-4'} . '")';
                 if ($mysqli->query($sql) === TRUE) {
                     http_response_code(200); // success
                 } else {
@@ -309,7 +318,7 @@ else {
             }
 
             for($i = 0; $i < count($attack); $i++) {
-                $sql = 'INSERT INTO attack (playerID, gameID, set1-0, set1-1, set1-2, set1-3, set2-0, set2-1, set2-2, set2-3, set3-0, set3-1, set3-2, set3-3, set4-0, set4-1, set4-2, set4-3, set5-0, set5-1, set5-2, set5-3) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $attack[$i]->{'s1-0'} . '", "' . $attack[$i]->{'s1-1'} . '", "' . $attack[$i]->{'s1-2'} . '", "' . $attack[$i]->{'s1-3'} . '", "' . $attack[$i]->{'s2-0'} . '", "' . $attack[$i]->{'s2-1'} . '", "' . $attack[$i]->{'s2-2'} . '", "' . $attack[$i]->{'s2-3'} . '", "' . $attack[$i]->{'s3-0'} . '", "' . $attack[$i]->{'s3-1'} . '", "' . $attack[$i]->{'s3-2'} . '", "' . $attack[$i]->{'s3-3'} . '", "' . $attack[$i]->{'s4-0'} . '", "' . $attack[$i]->{'s4-1'} . '", "' . $attack[$i]->{'s4-2'} . '", "' . $attack[$i]->{'s4-3'} . '", "' . $attack[$i]->{'s5-0'} . '", "' . $attack[$i]->{'s5-1'} . '", "' . $attack[$i]->{'s5-2'} . '", "' . $attack[$i]->{'s5-3'} . '")';
+                $sql = 'INSERT INTO attack (playerID, gameID, set10, set11, set12, set13, set20, set21, set22, set23, set30, set31, set32, set33, set40, set41, set42, set43, set50, set51, set52, set53) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $attack[$i]->{'s1-0'} . '", "' . $attack[$i]->{'s1-1'} . '", "' . $attack[$i]->{'s1-2'} . '", "' . $attack[$i]->{'s1-3'} . '", "' . $attack[$i]->{'s2-0'} . '", "' . $attack[$i]->{'s2-1'} . '", "' . $attack[$i]->{'s2-2'} . '", "' . $attack[$i]->{'s2-3'} . '", "' . $attack[$i]->{'s3-0'} . '", "' . $attack[$i]->{'s3-1'} . '", "' . $attack[$i]->{'s3-2'} . '", "' . $attack[$i]->{'s3-3'} . '", "' . $attack[$i]->{'s4-0'} . '", "' . $attack[$i]->{'s4-1'} . '", "' . $attack[$i]->{'s4-2'} . '", "' . $attack[$i]->{'s4-3'} . '", "' . $attack[$i]->{'s5-0'} . '", "' . $attack[$i]->{'s5-1'} . '", "' . $attack[$i]->{'s5-2'} . '", "' . $attack[$i]->{'s5-3'} . '")';
                 if ($mysqli->query($sql) === TRUE) {
                     http_response_code(200); // success
                 } else {
@@ -318,7 +327,7 @@ else {
             }
 
             for($i = 0; $i < count($block); $i++) {
-                $sql = 'INSERT INTO block (playerID, gameID, set1-0, set1-1, set2-0, set2-1, set3-0, set3-1, set4-0, set4-1, set5-0, set5-1) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $block[$i]->{'s1-0'} . '", "' . $block[$i]->{'s1-1'} . '", "' . $block[$i]->{'s2-0'} . '", "' . $block[$i]->{'s2-1'} . '", "' . $block[$i]->{'s3-0'} . '", "' . $block[$i]->{'s3-1'} . '", "' . $block[$i]->{'s4-0'} . '", "' . $block[$i]->{'s4-1'} . '", "' . $block[$i]->{'s5-0'} . '", "' . $block[$i]->{'s5-1'} . '")';
+                $sql = 'INSERT INTO block (playerID, gameID, set10, set11, set20, set21, set30, set31, set40, set41, set50, set51) VALUES ("' . $playerID[$i] . '", "' . $gameID . '", "' . $block[$i]->{'s1-0'} . '", "' . $block[$i]->{'s1-1'} . '", "' . $block[$i]->{'s2-0'} . '", "' . $block[$i]->{'s2-1'} . '", "' . $block[$i]->{'s3-0'} . '", "' . $block[$i]->{'s3-1'} . '", "' . $block[$i]->{'s4-0'} . '", "' . $block[$i]->{'s4-1'} . '", "' . $block[$i]->{'s5-0'} . '", "' . $block[$i]->{'s5-1'} . '")';
                 if ($mysqli->query($sql) === TRUE) {
                     http_response_code(200); // success
                 } else {
@@ -326,7 +335,8 @@ else {
                 }
             }
 
-            $sql = 'INSERT INTO setting (gameID, set1-1, set1-2, set1-3, set1-4, set1-5, set1-6, set2-1, set2-2, set2-3, set2-4, set2-5, set2-6, set3-1, set3-2, set3-3, set3-4, set3-5, set3-6, set4-1, set4-2, set4-3, set4-4, set4-5, set4-6, set5-1, set5-2, set5-3, set5-4, set5-5, set5-6) VALUES ("' . $gameID . '", "' . $setting->{'s1-1'} . '","' . $setting->{'s1-2'} . '","' . $setting->{'s1-3'} . '","' . $setting->{'s1-4'} . '","' . $setting->{'s1-5'} . '","' . $setting->{'s1-6'} . '","' . $setting->{'s2-1'} . '","' . $setting->{'s2-2'} . '","' . $setting->{'s2-3'} . '","' . $setting->{'s2-4'} . '","' . $setting->{'s2-5'} . '","' . $setting->{'s2-6'} . '","' . $setting->{'s3-1'} . '","' . $setting->{'s3-2'} . '","' . $setting->{'s3-3'} . '","' . $setting->{'s3-4'} . '","' . $setting->{'s3-5'} . '","' . $setting->{'s3-6'} . '","' . $setting->{'s4-1'} . '","' . $setting->{'s4-2'} . '","' . $setting->{'s4-3'} . '","' . $setting->{'s4-4'} . '","' . $setting->{'s4-5'} . '","' . $setting->{'s4-6'} . '","' . $setting->{'s5-1'} . '","' . $setting->{'s5-2'} . '","' . $setting->{'s5-3'} . '","' . $setting->{'s5-4'} . '","' . $setting->{'s5-5'} . '","' . $setting->{'s5-6'} . '")';
+            $sql = 'INSERT INTO setting (gameID, set11, set12, set13, set14, set15, set16, set21, set22, set23, set24, set25, set26, set31, set32, set33, set34, set35, set36, set41, set42, set43, set44, set45, set46, set51, set52, set53, set54, set55, set56) VALUES ("' . $gameID . '", "' . $setting[0]->{'s1-1'} . '","' . $setting[0]->{'s1-2'} . '","' . $setting[0]->{'s1-3'} . '","' . $setting[0]->{'s1-4'} . '","' . $setting[0]->{'s1-5'} . '","' . $setting[0]->{'s1-6'} . '","' . $setting[0]->{'s2-1'} . '","' . $setting[0]->{'s2-2'} . '","' . $setting[0]->{'s2-3'} . '","' . $setting[0]->{'s2-4'} . '","' . $setting[0]->{'s2-5'} . '","' . $setting[0]->{'s2-6'} . '","' . $setting[0]->{'s3-1'} . '","' . $setting[0]->{'s3-2'} . '","' . $setting[0]->{'s3-3'} . '","' . $setting[0]->{'s3-4'} . '","' . $setting[0]->{'s3-5'} . '","' . $setting[0]->{'s3-6'} . '","' . $setting[0]->{'s4-1'} . '","' . $setting[0]->{'s4-2'} . '","' . $setting[0]->{'s4-3'} . '","' . $setting[0]->{'s4-4'} . '","' . $setting[0]->{'s4-5'} . '","' . $setting[0]->{'s4-6'} . '","' . $setting[0]->{'s5-1'} . '","' . $setting[0]->{'s5-2'} . '","' . $setting[0]->{'s5-3'} . '","' . $setting[0]->{'s5-4'} . '","' . $setting[0]->{'s5-5'} . '","' . $setting[0]->{'s5-6'} . '")';
+
             if ($mysqli->query($sql) === TRUE) {
                 http_response_code(200); // success
             } else {
